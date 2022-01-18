3d15c5a2f8b67e184b6fb7f455120ad4a1742179
Jack Pena <penajackc@gmail.com>
2022-01-18 15:58:09 -0700

    commit tree 01d2576a7f6815b26363244c3aaf492aad5b052a

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(user, emailaddy){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: user,
+            email: emailaddy
+        })
+    }).then(res => res.json())
+    .then(data => document.body.append(data.id))
+    .catch(function(error){
+        document.body.append(error.message)
+    })
+}
